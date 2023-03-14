53c284fee72274875f76c9a4527479571e3527cc
Degner3 <niklaschristiansen@live.dk>
2023-03-14 12:23:37 +0100

    eks

--- JS/eksempel.js
+++ JS/eksempel.js
@@ -1,8 +1,19 @@
 
 
 $(function(){
-    $('button').click(function () {
-        $('.kasse').toggleClass('kasse-rotate');
+    $('.knap-one').click(function () {
+        console.log("hey");
+        $('.kasse1').toggleClass('kasse-rotate');
+      
+    });
+
+    $('.knap2').click(function () {
+        $('.kasse2').toggleClass('kasse-rotate');
+      
+    });
+
+    $('.knap3').click(function () {
+        $('.kasse3').toggleClass('kasse-rotate');
       
     });
 })
